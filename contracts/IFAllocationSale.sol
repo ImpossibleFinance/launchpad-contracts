b20f69b73aef74924b2af1bf0e2506ba0c6ae0c0
friday-james <lamcollin37@gmail.com>
2022-09-12 23:04:04 -0700

    Remove dead code

--- contracts/IFAllocationSale.sol
+++ contracts/IFAllocationSale.sol
@@ -718,17 +718,4 @@ contract IFAllocationSale is Ownable, ReentrancyGuard {
 
         return saleTokenOwed;
     }
-
-    function _hashPair(bytes32 a, bytes32 b) private pure returns (bytes32) {
-        return a < b ? _efficientHash(a, b) : _efficientHash(b, a);
-    }
-
-    function _efficientHash(bytes32 a, bytes32 b) private pure returns (bytes32 value) {
-        /// @solidity memory-safe-assembly
-        assembly {
-            mstore(0x00, a)
-            mstore(0x20, b)
-            value := keccak256(0x00, 0x40)
-        }
-    }
 }
