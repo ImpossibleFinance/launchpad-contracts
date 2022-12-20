9c4dcd9d49672fed5270ee84700fb59b8d7e4ff7
Friday James <57208185+friday-james@users.noreply.github.com>
2022-12-20 04:24:39 +0100

    Add script to generate go contracts (#92)
    * Add abigen script
    
    * Install dependencies
    
    * Trigger on main branch only
    
    * Add script to generate go contracts

---
+++ scripts/compile-go-file.sh
@@ -0,0 +1,3 @@
+npx hardhat export-abi
+docker run -v $PWD:/sources ethereum/solc:0.8.9 --optimize-runs=200 --bin /sources/contracts/$1.sol --include-path /sources/node_modules/ --base-path /sources -o /sources/$1.bin --overwrite
+abigen --abi=abi/contracts/$1.sol/$1.json --pkg=$1 --out=go-contracts/$1.go --bin $1.bin/$1.bin
