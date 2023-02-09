50b4250a75570d6df3ac606c35ee8b6e6d36a2ed
SeelyHamster <seelyhamster@impossible.finance>
2023-02-09 15:07:12 +0700

    Add getBatchStakeWeightByTrackId

--- contracts/IFAllocationMaster.sol
+++ contracts/IFAllocationMaster.sol
@@ -78,6 +78,12 @@ contract IFAllocationMaster is
         uint104 maxTotalStake;
     }
 
+    // Info of each user stake weight.
+    struct AddressStakeWeight {
+        address user;
+        uint192 stakeWeight;
+    }
+
     // INFO FOR FACTORING IN ROLLOVERS
 
     // the number of checkpoints of a track -- (track, finished sale count) => timestamp number
@@ -435,6 +441,32 @@ contract IFAllocationMaster is
 
         // return
         return stakeWeight;
+    }
+
+    // get batch stake weight users by trackId
+    function getBatchStakeWeightByTrackId(uint24 trackId, uint80 timestamp, uint start, uint count)
+        public
+        view
+        returns (AddressStakeWeight[] memory)
+    {   
+        AddressStakeWeight[] memory result;
+
+        // total current user for a track
+        uint256 totalUser = numTrackStakers[trackId];
+
+        uint end = start + count;
+        uint stakersCount = totalUser < end ? totalUser : end;
+
+        for (uint i = start; i < stakersCount; i++) {
+            // get staker address
+            address staker = trackStakers[trackId][i];
+            // get stake weight for user
+            uint192 stakeWeight = getUserStakeWeight(trackId, staker, timestamp);
+        
+            result[i - start] = AddressStakeWeight(staker, stakeWeight);
+        }
+
+        return result;
     }
 
     // get closest PRECEDING track checkpoint
