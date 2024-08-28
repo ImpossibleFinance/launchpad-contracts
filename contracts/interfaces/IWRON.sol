dfd729f80ff197e3ee0a5b63540283b4c19387f8
friday-james <lamcollin37@gmail.com>
2024-08-27 20:11:44 -0700

    linting

--- contracts/interfaces/IWRON.sol
+++ contracts/interfaces/IWRON.sol
@@ -2,17 +2,24 @@
 pragma solidity ^0.8.0;
 
 interface IWRON {
-  function deposit() external payable;
+    function deposit() external payable;
 
-  function transfer(address to, uint256 value) external returns (bool);
+    function transfer(address to, uint256 value) external returns (bool);
 
-  function transferFrom(address src, address dst, uint256 wad) external returns (bool);
+    function transferFrom(
+        address src,
+        address dst,
+        uint256 wad
+    ) external returns (bool);
 
-  function withdraw(uint256) external;
+    function withdraw(uint256) external;
 
-  function balanceOf(address) external view returns (uint256);
+    function balanceOf(address) external view returns (uint256);
 
-  function approve(address guy, uint256 wad) external returns (bool);
+    function approve(address guy, uint256 wad) external returns (bool);
 
-  function allowance(address owner, address spender) external view returns (uint256);
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
 }
--- contracts/interfaces/IWRONHelper.sol
+++ contracts/interfaces/IWRONHelper.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.0;
 
 interface IWRONHelper {
-  function WRON() external view returns (address);
+    function WRON() external view returns (address);
 
-  function withdraw(uint256 _amount) external;
+    function withdraw(uint256 _amount) external;
 
-  function withdrawTo(address payable _to, uint256 _amount) external;
+    function withdrawTo(address payable _to, uint256 _amount) external;
 }
